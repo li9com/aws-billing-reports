5dfda8d9207c3d9642f88ef8ed75aad78369f438
Vasily Angapov <angapov@gmail.com>
2019-10-18 13:07:30 +0530

    Add buildspec.yml for AWS CodeBuild and one more query

--- docker/queries.sql
+++ docker/queries.sql
@@ -2,13 +2,14 @@ SET @end   = DATE_FORMAT(NOW(), '%Y-%m-%d 00:00:00');
 SET @start = DATE_FORMAT(NOW() - INTERVAL ${REPORT_INTERVAL_DAYS} DAY, '%Y-%m-%d 00:00:00');
 
 SELECT  CONCAT('REPORT FROM ', @start, ' TO ', @end) AS '';
-SELECT  ROUND(SUM(`unblendedCost`), 2) AS 'TOTAL COST',
+SELECT  ROUND(SUM(`unblendedCost`), 2) AS 'Total unblended cost',
         `LinkedAccountId` AS 'AccountID'
         FROM report
         WHERE `UsageStartDate` >= @start
           AND `UsageEndDate`   <= @end
         GROUP BY `LinkedAccountId`;
-SELECT  'Cost by service' AS '';
+
+\! echo "Cost by service"
 SELECT  ROUND(SUM(`unblendedCost`), 2) cost,
         `usageType`,
         `availabilityZone` AS 'AZ',
@@ -19,7 +20,7 @@ SELECT  ROUND(SUM(`unblendedCost`), 2) cost,
         GROUP BY `usageType`,`availabilityZone`, `LinkedAccountId`
         HAVING SUM(`unblendedCost`) > 0.01;
 
-SELECT  'Cost by owner' AS '';
+\! echo "Cost by owner"
 SELECT  ROUND(SUM(`UnBlendedCost`), 2) AS cost,
         `aws:createdBy`,
         `user:Name` AS 'tag:Name',
@@ -29,3 +30,13 @@ SELECT  ROUND(SUM(`UnBlendedCost`), 2) AS cost,
           AND `UsageEndDate`   <= @end
         GROUP BY `aws:createdBy`, `user:Name`, `LinkedAccountId`
         HAVING SUM(`UnBlendedCost`) > 0.01;
+
+\! echo "Cost by date"
+SELECT  ROUND(SUM(`unblendedCost`), 2) cost,
+        DATE(`UsageStartDate`),
+        `LinkedAccountId` AS 'AccountID'
+        FROM report
+        WHERE `UsageStartDate` >= @start
+          AND `UsageEndDate`   <= @end
+        GROUP BY DATE(`UsageStartDate`), `LinkedAccountId`
+        HAVING SUM(`unblendedCost`) > 0.01;
